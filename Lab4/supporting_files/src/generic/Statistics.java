dc474e98dbf4e7544455a792bad61e9f31161ab1
Prithish Chaudhary <chaudharyprithish05@gmail.com>
2026-03-02 15:42:29 +0530

    Lab4

---
+++ Lab4/supporting_files/src/generic/Statistics.java
@@ -0,0 +1,86 @@
+package generic;
+
+import java.io.PrintWriter;
+
+public class Statistics {
+	
+	// TODO add your statistics here
+	static int numberOfInstructions;
+	static int numberOfCycles;
+	static float ipc;
+	static float cpi;
+    static int dataHazardStalls;
+    static int wrongBranchCount;
+
+	public static void printStatistics(String statFile)
+	{
+		try
+		{
+			PrintWriter writer = new PrintWriter(statFile);
+			
+			writer.println("Number of instructions executed = " + numberOfInstructions);
+			writer.println("Number of cycles taken = " + numberOfCycles);
+			
+			// TODO add code here to print statistics in the output file
+			writer.println("IPC = " + ipc);
+			writer.println("CPI = " + cpi);
+            writer.println("Number of Data Hazard Stalls = " + dataHazardStalls);
+            writer.println("Number of Wrong Branch Instructions that entered the pipeline = " + 2*wrongBranchCount);
+			writer.close();
+		}
+		catch(Exception e)
+		{
+			Misc.printErrorAndExit(e.getMessage());
+		}
+	}
+	
+	// TODO write functions to update statistics
+
+
+	//getters and setters of inst_num, cycle_num, cpi_ipc (ALL STATISTICS)
+
+	public static void setNumberOfInstructions(int numberOfInstructions) {
+		Statistics.numberOfInstructions = numberOfInstructions;
+	}
+
+	public static void setNumberOfCycles(int numberOfCycles) {
+		Statistics.numberOfCycles = numberOfCycles;
+	}
+
+	public static int getNumberofInstruction(){
+		return numberOfInstructions;
+	}
+    public static void incrementInstructions(){
+        numberOfInstructions++;
+    }
+	public static int getNumberofCycles(){
+		return numberOfCycles;
+	}
+	public static void setIpc(int instruction_num, int cycles_num){
+		ipc = (float)instruction_num/(float)cycles_num;
+	}
+	public static void setCpi(int instruction_num, int cycles_num) {
+		cpi = (float)cycles_num / (float)instruction_num;
+	}
+
+	public static float getIpc() {
+		return ipc;
+	}
+
+	public static float getCpi() {
+		return cpi;
+	}
+    public static int getDataHazardStalls() {
+        return dataHazardStalls;
+    }
+    public static void incrementDataHazardStalls() {
+        dataHazardStalls++;
+    }
+    public static int getWrongBranchCount() {
+        return wrongBranchCount;
+    }
+    public static void incrementWrongBranchCount() {
+        wrongBranchCount++;
+    }
+
+}
---
+++ Lab4/supporting_files/src/processor/pipeline/RegisterWrite.java
@@ -0,0 +1,60 @@
+package processor.pipeline;
+
+import generic.*;
+import processor.Processor;
+
+public class RegisterWrite {
+	Processor containingProcessor;
+	MA_RW_LatchType MA_RW_Latch;
+	IF_EnableLatchType IF_EnableLatch;
+
+	public RegisterWrite(Processor containingProcessor, MA_RW_LatchType mA_RW_Latch,
+			IF_EnableLatchType iF_EnableLatch) {
+		this.containingProcessor = containingProcessor;
+		this.MA_RW_Latch = mA_RW_Latch;
+		this.IF_EnableLatch = iF_EnableLatch;
+	}
+
+	public void performRW() {
+		if (MA_RW_Latch.isRW_enable()) {
+
+			// if instruction being processed is an end instruction, remember to call
+			// Simulator.setSimulationComplete(true);
+
+			// Taking instruction again from latch(MA_RW_Latch)
+			Instruction instruction = MA_RW_Latch.getInstruction();
+			String op = instruction.getOperationType().toString();
+
+			if (op.equals("load")) {
+				// Putting load value into rd
+				// Setting (register number, value)
+				int rd = instruction.getDestinationOperand().getValue();
+				containingProcessor.getRegisterFile().setValue(rd, MA_RW_Latch.getIdresult());
+				containingProcessor.getRegisterFile().setRegisterBusy(rd, false);
+			}
+
+			else if (op.equals("store") || op.equals("jmp") || op.equals("beq") || op.equals("blt") || op.equals("bne")
+					|| op.equals("bgt")) {
+				// Do nothing
+			}
+
+			else if (op.equals("end")) {
+				Simulator.setSimulationComplete(true);
+			} else {
+				// Putting alu result into rd (register number, value)
+				int rd = instruction.getDestinationOperand().getValue();
+				containingProcessor.getRegisterFile().setValue(rd, MA_RW_Latch.getALu());
+				containingProcessor.getRegisterFile().setRegisterBusy(rd, false);
+				// div/divi also wrote remainder to x31 — clear its busy flag
+				if (op.equals("div") || op.equals("divi")) {
+					containingProcessor.getRegisterFile().setRegisterBusy(31, false);
+				}
+			}
+
+			Statistics.incrementInstructions();
+
+			MA_RW_Latch.setRW_enable(false);
+		}
+	}
+
+}
